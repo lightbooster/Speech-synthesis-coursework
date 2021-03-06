66b0aa0732fe3eaa9768975737a46fe448490a44
Michail <mdmurzinov@edu.hse.ru>
2021-03-06 11:49:14 +0300

    number to words translation

--- russian_preprocessing.py
+++ russian_preprocessing.py
@@ -1,3 +1,22 @@
+import inflect
+from googletrans import Translator
+
+
+class MyTranslator:
+    def __init__(self):
+        # Some code
+        self.translator = Translator()
+        # Some code
+
+    def translate(self, in_text, in_lang="en", out_lang="ru"):
+        while True:
+            try:
+                result_ = self.translator.translate(in_text, src=in_lang, dest=out_lang)
+                return result_
+            except Exception as e:
+                self.translator = Translator()
+                print(e, "<---- Ошибка")
+
 
 class RussianPreprocessor:
     """
@@ -8,6 +27,8 @@ class RussianPreprocessor:
 
     def __init__(self):
         self._symbols_dict = {self.__SYMBOLS[i]: i for i in range(len(self.__SYMBOLS))}
+        self._my_translator = MyTranslator()
+        self._inflect_engine = inflect.engine()
 
     def _preprocess_text(self, data: str):
         """
@@ -44,3 +65,16 @@ class RussianPreprocessor:
         encoded_chars = [self._symbols_dict[char] for char in preprocessed_data]
         return encoded_chars
 
+    def number_to_text(self, number: int, in_lang_="en", out_lang_="ru") -> str:
+        string_num = self._inflect_engine.number_to_words(number)
+        result = self._my_translator.translate(in_text=string_num, in_lang=in_lang_, out_lang=out_lang_)
+        return result.text
+
+
+def main() -> None:
+    my_rp = RussianPreprocessor()
+    # print(my_rp.number_to_text(151))
+
+
+if __name__ == '__main__':
+    main()
